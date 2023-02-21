95afdb608b9207f5dd9a8d46e3de0fc7f925aac0
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-02-21 19:06:46 +0530

    implemented comment change,  responsivness

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const equalBtn = document.getElementById("equal");
 const clearBtn = document.getElementById("clear");
 let num1 = null;
 let num2 = null;
-let op = null;
+let mainOperator = null;
 console.log("render");
+//Handle Clicks on Operators and Numbers
 document.addEventListener("click", handleClick);
-
 function handleClick(e) {
   if (e.target.classList.contains("number")) {
     handleNumber(parseInt(e.target.value));
@@ -15,7 +15,7 @@ function handleClick(e) {
   }
 }
 function handleNumber(num) {
-  if (op === null) {
+  if (mainOperator === null) {
     if (num1 === null) num1 = num;
     else {
       num1 *= 10;
@@ -28,7 +28,7 @@ function handleNumber(num) {
       num2 *= 10;
       num2 += num;
     }
-    display.textContent = num1 + op + num2;
+    display.textContent = num1 + mainOperator + num2;
   }
 }
 function handleOperator(operator) {
@@ -36,25 +36,27 @@ function handleOperator(operator) {
     display.textContent = "Enter Number First";
     return;
   }
-  op = operator;
-  display.textContent = num1 + op;
+  if (num2 !== null) handleEqual();
+  mainOperator = operator;
+  display.textContent = num1 + mainOperator;
 }
+//handle Click on Equal Button
 equalBtn.addEventListener("click", handleEqual);
 function handleEqual() {
-  if (num1 === null || op === null || num2 === null) return;
-  if (op === "+") num1 = num1 + num2;
-  else if (op === "-") num1 -= num2;
-  else if (op === "*") num1 *= num2;
-  else if (op === "/") num1 /= num2;
+  if (num1 === null || mainOperator === null || num2 === null) return;
+  if (mainOperator === "+") num1 = num1 + num2;
+  else if (mainOperator === "-") num1 -= num2;
+  else if (mainOperator === "*") num1 *= num2;
+  else if (mainOperator === "/") num1 /= num2;
   num2 = null;
-  op = null;
+  mainOperator = null;
   display.textContent = num1;
 }
-
+//Handle Clear Button
 clearBtn.addEventListener("click", handleClear);
 function handleClear() {
   num1 = null;
-  op = null;
+  mainOperator = null;
   num2 = null;
   display.textContent = 0;
 }
